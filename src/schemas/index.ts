61769aaf8e8eafda0721b346ab2f3b63600c6e29
David <daviolivero2103@gmail.com>
2026-06-08 22:25:40 -0400

    chore: create register schema

---
+++ src/schemas/index.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const RegisterSchema = z.object({
+    email: z.string()
+            .min(1, {message: 'Email is required'})
+            .pipe(z.email({ message: "Invalid email address" })),
+    name: z.string().min(1, {message: 'The name cannot be empty'}),
+    password: z.string().min(8, {message: 'The password is very short, it must have minimum of 8 characters'}),
+    password_confirmation: z.string()
+}).refine((data) => data.password === data.password_confirmation, {
+    message: 'The passwords are not the same'
+});
+
