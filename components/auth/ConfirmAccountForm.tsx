03e66ee55d8dc58dce8f656cccef303381fa837e
David <daviolivero2103@gmail.com>
2026-06-10 13:25:58 -0400

    chore: save token in state

--- components/auth/ConfirmAccountForm.tsx
+++ components/auth/ConfirmAccountForm.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import { PinInputField, PinInput } from "@chakra-ui/pin-input";
+import { useState } from "react";
 
 export default function ConfirmAccountForm() {
+    const [token, setToken] = useState('');
+
+    const handleChange = (token: string) => {
+        setToken(token);
+        console.log(token);
+    };
+
+    const handleComplete = () => {
+        console.log('Llegaste al final');
+    };
+
     return (
         <div className="flex justify-center gap-5 mt-10">
-            <PinInput>
+            <PinInput onChange={handleChange} value={token} onComplete={handleComplete}>
                 <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
                 <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
                 <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
