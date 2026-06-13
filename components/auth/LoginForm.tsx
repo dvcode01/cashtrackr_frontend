5243c2c39fe6a8c7a88136c871134d5f33bed712
David <daviolivero2103@gmail.com>
2026-06-13 17:34:52 -0400

    chore: display authentication errors

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -2,6 +2,7 @@
 
 import { authenticate } from "@/actions/authenticate-user-action";
 import { useActionState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 export default function LoginForm() {
     const [state, dispatch] = useActionState(authenticate, {
@@ -9,7 +10,9 @@ export default function LoginForm() {
     });
 
     useEffect(() => {
-        console.log(state)
+        if(state.errors){
+            state.errors.forEach(error => toast.error(error));
+        }
 
     }, [state]);
 
