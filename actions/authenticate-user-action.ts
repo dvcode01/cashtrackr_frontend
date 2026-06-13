0fb835661040b205961e68192d66674dace73258
David <daviolivero2103@gmail.com>
2026-06-13 17:56:24 -0400

    chore: send login data to backend

--- actions/authenticate-user-action.ts
+++ actions/authenticate-user-action.ts
@@ -20,6 +20,22 @@ export async function authenticate(prevState: ActionStateType, formData: FormDat
         }
     }
 
+    // autenticacion
+    const url = `${process.env.API_URL}/auth/login`;
+    const req = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: auth.data.email,
+            password: auth.data.password
+        })
+    });
+
+    const json = await req.json();
+    console.log(json);
+
     return {
         errors: []
     }
