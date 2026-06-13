ed1f35517e9a336ff815260bccefe5bc36544d3b
David <daviolivero2103@gmail.com>
2026-06-12 23:06:28 -0400

    feat: send data to backend

--- actions/confirm-account-action.ts
+++ actions/confirm-account-action.ts
@@ -1,15 +1,13 @@
 "use server";
 
-import { TokenSchema } from "@/src/schemas";
+import { TokenSchema, ErrorResponse, SuccessSchema } from "@/src/schemas";
 
 type ActionStateType = {
     errors: string[],
+    success: string
 };
 
 export async function confirmAccount(token: string, prevState: ActionStateType){
-    console.log('Desde confirm account');
-    console.log(token);
-
     const confirmToken = TokenSchema.safeParse(token);
 
     if(!confirmToken.success){
@@ -19,10 +17,34 @@ export async function confirmAccount(token: string, prevState: ActionStateType){
         };
     }
 
-    console.log(confirmToken.data);
+    // Enviando token para confirmacion
+    const url = `${process.env.API_URL}/auth/confirm-account`;
+    const req = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            
+        },
+        body: JSON.stringify({
+            token: confirmToken.data
+        })
+    });
 
+    const json = await req.json();
+
+    if(!req.ok){
+        const { error } = ErrorResponse.parse(json);
+    
+        return {
+            errors: [error],
+            success: ''
+        };
+    }
+
+    const success = SuccessSchema.parse(json);
     return {
-        errors: []
+        errors: [],
+        success
     };
 }
 
--- components/auth/ConfirmAccountForm.tsx
+++ components/auth/ConfirmAccountForm.tsx
@@ -3,6 +3,8 @@
 import { confirmAccount } from "@/actions/confirm-account-action";
 import { PinInputField, PinInput } from "@chakra-ui/pin-input";
 import { startTransition, useActionState, useEffect, useState } from "react";
+import ErrorMessage from "../ui/ErrorMessage";
+import SuccessMessage from "../ui/SuccessMessage";
 
 export default function ConfirmAccountForm() {
     const [isComplete, setIsComplete] = useState(false);
@@ -10,7 +12,8 @@ export default function ConfirmAccountForm() {
 
     const confirmAccountWithToken = confirmAccount.bind(null, token);
     const [state, dispatch] = useActionState(confirmAccountWithToken, {
-        errors: []
+        errors: [],
+        success: ''
     });
 
     useEffect(() => {
@@ -30,16 +33,22 @@ export default function ConfirmAccountForm() {
     };
 
     return (
-        <div className="flex justify-center gap-5 mt-10">
-            <PinInput onChange={handleChange} value={token} onComplete={handleComplete}>
-                <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
-                <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
-                <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
-                <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
-                <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
-                <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
-            </PinInput>
-        </div>
+        <>
+            {state.errors.map(error => <ErrorMessage>{error}</ErrorMessage>)}
+            {state.success && <SuccessMessage>{state.success}</SuccessMessage>}
+
+            <div className="flex justify-center gap-5 mt-10">
+                <PinInput onChange={handleChange} value={token} onComplete={handleComplete}>
+                    <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
+                    <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
+                    <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
+                    <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
+                    <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
+                    <PinInputField className="w-10 h-10 rounded-lg shadow border border-gray-300 placeholder-white text-center" />
+                </PinInput>
+            </div>
+        
+        </>
     )
 }
 
