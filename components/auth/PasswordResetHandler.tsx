c88e825debfacaff6c25671aed53ac846609f858
David <daviolivero2103@gmail.com>
2026-06-15 15:38:53 -0400

    chore: add password handler component

---
+++ components/auth/PasswordResetHandler.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react"
+import ResetPasswordForm from "./ResetPasswordForm";
+import ValidateTokenForm from "./ValidateTokenForm";
+
+
+export default function PasswordResetHandler() {
+    const [isValidateToken, setIsValidateToken] = useState(false);
+
+    return (
+        <>
+            {!isValidateToken ? <ValidateTokenForm /> : <ResetPasswordForm /> }
+        </>
+    )
+}
