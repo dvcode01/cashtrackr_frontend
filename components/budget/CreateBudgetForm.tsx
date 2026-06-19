73107c4b3793a37da005a03e58240e4fe9978b66
David <daviolivero2103@gmail.com>
2026-06-18 21:47:01 -0400

    chore: move input fields in new component

--- components/budget/CreateBudgetForm.tsx
+++ components/budget/CreateBudgetForm.tsx
@@ -5,6 +5,7 @@ import { useActionState, useEffect } from "react";
 import ErrorMessage from "../ui/ErrorMessage";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
+import BudgetForm from "./BudgetForm";
 
 export default function CreateBudgetForm() {
     const router = useRouter();
@@ -29,30 +30,9 @@ export default function CreateBudgetForm() {
             noValidate
         >
             {state.errors.map(error => <ErrorMessage>{error}</ErrorMessage>)}
-            <div className="space-y-3">
-                <label htmlFor="name" className="text-sm uppercase font-bold">
-                    Nombre Presupuesto
-                </label>
-                <input
-                    id="name"
-                    className="w-full p-3 placeholder:text-slate-400 text-slate-400 border border-gray-100 bg-slate-100"
-                    type="text"
-                    placeholder="Nombre del Presupuesto"
-                    name="name"
-                />
-            </div>
-            <div className="space-y-3">
-                <label htmlFor="amount" className="text-sm uppercase font-bold">
-                    Cantidad Presupuesto
-                </label>
-                <input
-                    type="number"
-                    id="amount"
-                    className="w-full p-3 placeholder:text-slate-400 text-slate-400 border border-gray-100 bg-slate-100"
-                    placeholder="Cantidad Presupuesto"
-                    name="amount"
-                />
-            </div>
+            
+            <BudgetForm />
+            
             <input
                 type="submit"
                 className="bg-amber-500 w-full p-3 text-white uppercase font-bold hover:bg-amber-600 cursor-pointer transition-colors"
