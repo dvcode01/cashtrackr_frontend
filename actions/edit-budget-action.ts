87b97282e90f76b6c29f0a763ad87d797ea187ed
David <daviolivero2103@gmail.com>
2026-06-18 22:51:04 -0400

    chore: send edit data to backend

--- actions/edit-budget-action.ts
+++ actions/edit-budget-action.ts
@@ -1,13 +1,14 @@
 "use server";
 
-import { Budget, DraftBudgetSchema } from "@/src/schemas";
+import getToken from "@/src/auth/token";
+import { Budget, DraftBudgetSchema, ErrorResponse, SuccessSchema } from "@/src/schemas";
 
 type ActionStateType = {
     errors: string[],
     success: string
 };
 
-export async function editBudget(budgetID: Budget['id'],prevState: ActionStateType, formData: FormData) {
+export async function editBudget(budgetID: Budget['id'], prevState: ActionStateType, formData: FormData) {
     const budgetData = {
         name: formData.get('name'),
         amount: formData.get('amount')
@@ -22,8 +23,36 @@ export async function editBudget(budgetID: Budget['id'],prevState: ActionStateTy
         };
     }
 
+    const token = await getToken();
+    const url = `${process.env.API_URL}/budgets/${budgetID}`;
+
+    const req = await fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            name: budget.data.name,
+            amount: budget.data.amount
+        })
+    });
+
+    const json = await req.json();
+
+    if(!req.ok){
+        const { error } = ErrorResponse.parse(json);
+
+        return {
+            errors: [error],
+            success: ''
+        };
+    }
+
+    const success = SuccessSchema.parse(json);
+
     return {
         errors: [],
-        success: ''
+        success
     };
 }
--- components/budget/EditBudgetForm.tsx
+++ components/budget/EditBudgetForm.tsx
@@ -2,16 +2,26 @@
 
 import { Budget } from "@/src/schemas";
 import BudgetForm from "./BudgetForm";
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
 import { editBudget } from "@/actions/edit-budget-action";
 import ErrorMessage from "../ui/ErrorMessage";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 export default function EditBudgetForm({ budget }: { budget: Budget }) {
+    const router = useRouter();
     const editBudgetWithID = editBudget.bind(null, budget.id);
     const [state, dispatch] = useActionState(editBudgetWithID, {
         errors: [],
         success: ''
     });
+
+    useEffect(() => {
+        if(state.success){
+            toast.success(state.success);
+            router.push('/admin');
+        }
+    }, [state]);
 
     return (
         <form
