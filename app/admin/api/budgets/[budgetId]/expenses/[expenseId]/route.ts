8eb64275597c0d44869ed60011ebf6bee008732f
David <daviolivero2103@gmail.com>
2026-06-26 22:53:45 -0400

    chore: obtain expense data in api

--- app/admin/api/budgets/[budgetId]/expenses/[expenseId]/route.ts
+++ app/admin/api/budgets/[budgetId]/expenses/[expenseId]/route.ts
@@ -1,7 +1,23 @@
 import { verifySession } from "@/src/auth/dal";
+import getToken from "@/src/auth/token";
 
-export async function GET(){
+export async function GET(request: Request, {params}: {params: {budgetId: string, expenseId: string}}){
+    const { budgetId, expenseId } = await params;
     await verifySession();
 
-    return Response.json('hola mundo');
+    const token = await getToken();
+    const url = `${process.env.API_URL}/budgets/${budgetId}/expenses/${expenseId}`;
+
+    const req = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    const json = await req.json();
+
+    if(!req.ok){
+        return Response.json(json.error, {status: 403});
+    }
+
+    return Response.json(json);
 }
