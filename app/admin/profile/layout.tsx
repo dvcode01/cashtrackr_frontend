1e771bec5c69fa4aa04e5b0ddac92d46a8429569
David <daviolivero2103@gmail.com>
2026-07-01 19:52:52 -0400

    chore: add metadata in profile pages

--- app/admin/profile/layout.tsx
+++ app/admin/profile/layout.tsx
@@ -1,5 +1,11 @@
 import ProfileTabs from "@/components/profile/ProfileTabs";
 import ToastNotification from "@/components/ui/ToastNotification";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Cashtrackr - Perfil',
+    description: 'Configuración de perfil en Cashtrackr'
+};
 
 export default async function ProfileLayout({
     children,
